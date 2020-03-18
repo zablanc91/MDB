84f4ce925a01d52994781d90f9a1fad24990ec42
Carlo Zablan <zablanc91@hotmail.com>
2020-03-18 11:48:25 -0700

    added logic to handle SearchBar and pull data from API call

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
+import { APIkey } from '../config/keys';
+import axios from 'axios';
 
 const App = () => {
+    //need Hook for state to tell us what is searched, the results pulled from the API, and what the user selected/clicked
+    const [state, setState] = useState({
+        searchTerm: "",
+        results: [],
+        selected: {}
+    });
+
+    //function to update state when user inputs new search term
+    const handleInput = (e) => {
+        let searchTerm = e.target.value;
+        setState( (prevState) => {
+            return {...prevState, searchTerm};
+        });
+    }
+
+    //function to make API call after user hits enter in SearchBar
+    const search = (e) => {
+        if(e.key === "Enter"){
+            axios(APIkey + '&s=' + state.searchTerm).then((data) => {
+                console.log(data);
+            });
+        }
+    }
+
     return(
         <div>
             <header>
                 <h1>MDB placeholder</h1>
             </header>
             <main>
-                <SearchBar />
+                <SearchBar handleInput={handleInput} search={search} />
             </main>
         </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
     return  (
         <section className="searchbar-wrap">
-            <input type="text" className="searchbar" placeholder="Search for a movie, TV show, person..."></input>
+            <input 
+                type="text" 
+                className="searchbar" 
+                placeholder="Search for a movie, TV show, person..." onChange={props.handleInput}
+                onKeyPress={props.search} />
         </section>
     );
 }
